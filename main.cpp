0e52b9f439c9500df47ebdd8448fd74300b0f418
LJY <2562156161@qq.com>
2025-04-21 15:08:48 +0800

    Add database.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include"database.h"
+using namespace std;
+B_Plus_Tree<char[65],int> BPT("Data");
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        char op[20],s[65];
+        int val;
+        scanf("%s%s",op,s);
+        if(op[0]=='i')
+        {
+            scanf("%d",&val);
+            BPT.Insert(s,val);
+        }
+        else if(op[0]=='d')
+        {
+            scanf("%d",&val);
+            BPT.Delete(s,val);
+        }
+        else
+        {
+            if(auto res=BPT.Find(s); res.empty())
+                puts("null");
+            else
+            {
+                for(const auto t:res)
+                    printf("%d ",t);
+                puts("");
+            }
+        }
+    }
+
+    return 0;
+}
